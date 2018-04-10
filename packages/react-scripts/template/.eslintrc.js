6bb6c626134a0c04204b544b88450a73fa4789ff
Frank Tran <frank.tran@teamone-usa.com>
2018-04-09 17:15:34 -0700

    Resolve webpack aliases in eslint + add public folder to lint ignore

--- packages/react-scripts/template/.eslintrc.js
+++ packages/react-scripts/template/.eslintrc.js
@@ -1,10 +1,23 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
   env: {
     browser: true,
   },
   extends: ['react-app', 'airbnb'],
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'config/webpack.config.dev.js',
+      },
+    },
+  },
   // add your custom rules here
   rules: {
     'jsx-a11y/href-no-hash': 'off', // This rule is used in CRA but has been removed in the later version of jsx-a11y used by airbnb
@@ -16,6 +29,10 @@ module.exports = {
         jsx: 'never',
       },
     ],
+    // allow optionalDependencies
+    // 'import/no-extraneous-dependencies': ['error', {
+    //   'optionalDependencies': ['test/unit/index.js']
+    // }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
   },
