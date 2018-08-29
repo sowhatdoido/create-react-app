de79173f1e1db5a2d4bedbd4d8312e4ee7932c20
Frank Tran <frank.tran@teamone-usa.com>
2018-08-29 14:34:21 -0700

    Add airbnb eslint support

--- packages/react-scripts/template/src/App.jsx
+++ packages/react-scripts/template/src/App.jsx
@@ -3,14 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 'Welcome!',
+    };
+  }
+
   render() {
+    const { message } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>{message}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
